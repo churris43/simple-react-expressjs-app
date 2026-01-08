9a2ef88240340e27ae793a8f39e04e41a238072b
churris43 <churris43@hotmail.com>
2026-01-08 16:09:50 +1100

    Tailwindcss configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div><h1 className="text-3xl">App</h1></div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold underline">App</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
