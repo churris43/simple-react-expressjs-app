cb2a325e9165d977a532fe9bdaf1e00f62e4f5fd
churris43 <churris43@hotmail.com>
2026-01-13 10:38:30 +1100

    Styling the nav bar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,24 +1,47 @@
 import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   const linkClass = ({ isActive }: { isActive: boolean }): string =>
-    isActive ? "bg-black text-white" : "bg-white text-black";
+    isActive
+      ? "bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+      : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
 
   return (
     <>
-      <NavLink to="/" className={linkClass}>
-        Home
-      </NavLink>
-      <br />
-      <NavLink to="/applications" className={linkClass}>
-        Applications
-      </NavLink>
-      <br />
-      <NavLink to="/applications/new" className={linkClass}>
-        Add Application
-      </NavLink>
-      <br />
-      <br />
+      <nav className="bg-indigo-700 border-b border-indigo-500 sticky top-0 ">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="flex h-20 items-center justify-between">
+            <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+              <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
+                <img
+                  className="h-10 w-auto"
+                  src={logo}
+                  alt="Job Applications (React)"
+                />
+                <span className="hidden md:block text-white text-2xl font-bold ml-2">
+                  Job Applications React
+                </span>
+              </NavLink>
+              <div className="md:ml-auto">
+                <div className="flex space-x-2">
+                  <NavLink to="/" className={linkClass}>
+                    Home
+                  </NavLink>
+                  <br />
+                  <NavLink to="/applications" className={linkClass}>
+                    Applications
+                  </NavLink>
+                  <br />
+                  <NavLink to="/applications/new" className={linkClass}>
+                    Add Application
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
