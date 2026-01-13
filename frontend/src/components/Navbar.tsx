1f1b238972205acfea61a855bc224275163161dd
churris43 <churris43@hotmail.com>
2026-01-13 14:13:24 +1100

    MAking navbar always present

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-indigo-700 border-b border-indigo-500 sticky top-0 ">
+      <nav className="bg-indigo-700 border-b border-indigo-500 sticky top-0 z-10">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
